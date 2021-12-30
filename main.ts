2e27847e3b58865db92799c650f5cef350b2f4ef
Branislav Stojanović <branislav.stojanovic@blinking.id>
2021-12-30 15:11:22 +0100

    edit

--- main.ts
+++ main.ts
@@ -1,15 +1,11 @@
 const http = require('http');
-import * as mongoose from 'mongoose'
 import { App } from './app';
-import { Server, Socket } from "socket.io";
+import { Server} from "socket.io";
 import { SocketManager } from './socket-manager';
 
 
 (async function main() {
     try {
-        //connecting to database
-        await mongoose.connect('mongodb+srv://bane:sifrasifra@cluster0.xxfox.mongodb.net/pong_multiplayer_db?retryWrites=true&w=majority');
-        console.log('Connecting with database successful')
 
         //instantiating server
         const server = http.createServer(App);
